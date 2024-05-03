3b2fa948a130aec2ca9ff133cae90a329fd38fa2
unknown <hamza.srhiri@gmail.com>
2024-05-03 01:24:08 -0400

    fixed issues and added delete functionality

--- GroupDiscussionScreen.js
+++ GroupDiscussionScreen.js
@@ -1,131 +1,142 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Button, FlatList, TextInput, Alert, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, Button, FlatList, TextInput, Alert, TouchableOpacity, StyleSheet, KeyboardAvoidingView } from 'react-native';
 import firebase from "firebase/compat";
 import { db } from './firebase';
 
+const DiscussionItem = ({ discussion, onPress }) => (
+  <TouchableOpacity onPress={onPress}>
+    <View style={styles.forumItem}>
+      <Text style={styles.forumTitle}>{discussion.title}</Text>
+      <Text style={styles.forumInfo}>Posted by {discussion.author} - {discussion.timestamp?.toDate().toLocaleString()}</Text>
+      <Text style={styles.forumDescription}>{discussion.description}</Text>
+    </View>
+  </TouchableOpacity>
+);
+
 const GroupDiscussionScreen = ({ navigation }) => {
-    const [discussions, setDiscussions] = useState([]);
-    const [newDiscussionTitle, setNewDiscussionTitle] = useState('');
-    const [newDiscussionDescription, setNewDiscussionDescription] = useState('');
+  const [discussions, setDiscussions] = useState([]);
+  const [newDiscussionTitle, setNewDiscussionTitle] = useState('');
+  const [newDiscussionDescription, setNewDiscussionDescription] = useState('');
 
-    // Function to fetch discussions from Firestore
-    const fetchDiscussions = async () => {
-        try {
-            const discussionsRef = firebase.firestore().collection('discussions');
-            const snapshot = await discussionsRef.get();
-            const discussionsData = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-            setDiscussions(discussionsData);
-        } catch (error) {
-            console.error('Error fetching discussions:', error);
-        }
-    };
-    const handleForumPress = (forumId, forumName) => {
-        // Navigate to the detailed view of the forum using navigation.navigate
-        navigation.navigate('ForumDetailScreen', { forumId: forumId, forumName: forumName });
-    };
+  // Function to fetch discussions from Firestore
+  const fetchDiscussions = async () => {
+    try {
+      const discussionsRef = firebase.firestore().collection('discussions');
+      const snapshot = await discussionsRef.get();
+      const discussionsData = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+      setDiscussions(discussionsData);
+    } catch (error) {
+      console.error('Error fetching discussions:', error);
+    }
+  };
 
-    useEffect(() => {
-        fetchDiscussions();
-    }, []);
+  useEffect(() => {
+    fetchDiscussions();
+  }, []);
 
-    const createDiscussion = async () => {
-        try {
-            const discussionsRef = firebase.firestore().collection('discussions');
-            await discussionsRef.add({
-                title: newDiscussionTitle,
-                author: 'Current User', // Replace with actual user information
-                timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-                description: newDiscussionDescription,
-            });
-            // Fetch discussions again to update the list
-            fetchDiscussions();
-            // Reset input fields
-            setNewDiscussionTitle('');
-            setNewDiscussionDescription('');
-            // Show success message
-            Alert.alert('Discussion Created', 'Your discussion has been created successfully.');
-        } catch (error) {
-            console.error('Error creating discussion:', error);
-            Alert.alert('Error', 'Failed to create discussion. Please try again.');
-        }
-    };
+  const handleForumPress = (forumId, forumName) => {
+    // Navigate to the detailed view of the forum using navigation.navigate
+    navigation.navigate('ForumDetailScreen', { forumId: forumId, forumName: forumName });
+  };
 
-    return (
-<View style={{ padding: 20 }}>
-			<Text style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 10 }}>Discussion Forum</Text>
+  const createDiscussion = async () => {
+    try {
+      const discussionsRef = firebase.firestore().collection('discussions');
+      await discussionsRef.add({
+        title: newDiscussionTitle,
+        author: 'Current User', // Replace with actual user information
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        description: newDiscussionDescription,
+      });
+      // Fetch discussions again to update the list
+      fetchDiscussions();
+      // Reset input fields
+      setNewDiscussionTitle('');
+      setNewDiscussionDescription('');
+      // Show success message
+      Alert.alert('Discussion Created', 'Your discussion has been created successfully.');
+    } catch (error) {
+      console.error('Error creating discussion:', error);
+      Alert.alert('Error', 'Failed to create discussion. Please try again.');
+    }
+  };
 
-            <FlatList
-                data={discussions}
-                keyExtractor={(item) => item.id.toString()}
-                renderItem={({ item }) => (
-                    <TouchableOpacity onPress={() => handleForumPress(item.id, item.title)}>
-                        <View style={styles.forumItem}>
-                            <Text style={styles.forumTitle}>{item.title}</Text>
-                            <Text style={styles.forumInfo}>Posted by {item.author} - {item.timestamp?.toDate().toLocaleString()}</Text>
-                            <Text style={styles.forumDescription}>{item.description}</Text>
-                        </View>
-                    </TouchableOpacity>
-                )}
-            />
+  return (
+    <KeyboardAvoidingView style={styles.container} behavior="padding" keyboardVerticalOffset={100}>
+      <Text style={styles.title}>Discussion Forum</Text>
 
-            <View style={styles.createDiscussionSection}>
-                <TextInput
-                    placeholder="Enter discussion title"
-                    value={newDiscussionTitle}
-                    onChangeText={setNewDiscussionTitle}
-                    style={styles.input}
-                />
-                <TextInput
-                    placeholder="Enter discussion description"
-                    value={newDiscussionDescription}
-                    onChangeText={setNewDiscussionDescription}
-                    multiline
-                    numberOfLines={4}
-                    style={[styles.input, { marginBottom: 20 }]}
-                />
+      <FlatList
+        data={discussions}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <DiscussionItem
+            discussion={item}
+            onPress={() => handleForumPress(item.id, item.title)}
+          />
+        )}
+      />
 
-                <TouchableOpacity style={styles.createButton} onPress={createDiscussion} disabled={!newDiscussionTitle || !newDiscussionDescription}>
-                    <Text style={styles.createButtonText}>Create Discussion</Text>
-                </TouchableOpacity>
-            </View>
-        </View>
-    );
+      <View style={styles.createDiscussionSection}>
+        <TextInput
+          placeholder="Enter discussion title"
+          value={newDiscussionTitle}
+          onChangeText={setNewDiscussionTitle}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Enter discussion description"
+          value={newDiscussionDescription}
+          onChangeText={setNewDiscussionDescription}
+          multiline
+          numberOfLines={4}
+          style={[styles.input, { marginBottom: 20 }]}
+        />
+
+        <TouchableOpacity style={styles.createButton} onPress={createDiscussion} disabled={!newDiscussionTitle || !newDiscussionDescription}>
+          <Text style={styles.createButtonText}>Create Discussion</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#f0f0f0',
-        paddingHorizontal: 20,
-        paddingTop: 20,
-    },
-    title: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        marginBottom: 10,
-    },
-    forumItem: {
-        marginBottom: 20,
-        backgroundColor: 'white',
-        padding: 10,
-        borderRadius: 8,
-        borderWidth: 1,
-        borderColor: '#dcdcdc',
-    },
-    forumTitle: {
-        fontSize: 18,
-        fontWeight: 'bold',
-    },
-    forumInfo: {
-        fontSize: 14,
-        color: 'gray',
-        marginTop: 5,
-    },
-    forumDescription: {
-        marginTop: 5,
+  container: {
+    flex: 1,
+    backgroundColor: '#f0f0f0',
+    paddingHorizontal: 20,
+    paddingTop: 20,
+
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+	
+  },
+  forumItem: {
+    marginBottom: 20,
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: '#dcdcdc',
+  },
+  forumTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  forumInfo: {
+    fontSize: 14,
+    color: 'gray',
+    marginTop: 5,
+  },
+  forumDescription: {
+    marginTop: 5,
     },
     createDiscussionSection: {
         marginTop: 20,
+		paddingBottom: 30,
     },
     input: {
         height: 40,
